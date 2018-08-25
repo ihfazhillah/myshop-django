84b4b2fe1ea26b16802d1ce76ae4930190a4a3e9
IbnuAmin <mihfazhillah@gmail.com>
2018-08-25 23:08:09 +0700

    add cart object

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop'
+    'shop',
+    'cart',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -107,3 +108,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CART_SESSION_ID = 'cart'
