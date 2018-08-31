f32a2099b7e8d124be8fcae39a7286eba81eb970
IbnuAmin <mihfazhillah@gmail.com>
2018-08-31 21:29:12 +0700

    add admin

--- coupons/admin.py
+++ coupons/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Coupon
 
 # Register your models here.
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'active', 'valid_from', 'valid_to', 'discount']
+    list_filter = ['active', 'valid_from', 'valid_to']
+    search_fields = ['code']
+
+admin.site.register(Coupon, CouponAdmin)
